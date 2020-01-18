147fec6c34cf7561aaa07c9e706d2bbb7207dc40
Gergana Dzhumerkova <gergana.dzhumerkova@ft.com>
2020-01-18 19:03:02 +0000

    Added /article/<id> endpoint

--- api.py
+++ api.py
@@ -2,9 +2,10 @@ import flask
 from flask import request, jsonify
 import sqlite3
 
-
 app = flask.Flask(__name__)
-#app.config["DEBUG"] = True
+
+
+# app.config["DEBUG"] = True
 
 def dict_factory(cursor, row):
     d = {}
@@ -12,14 +13,27 @@ def dict_factory(cursor, row):
         d[col[0]] = row[idx]
     return d
 
-@app.route('/all', methods=['GET'])
-def home():
-  conn = sqlite3.connect('articles.db')
-  conn.row_factory = dict_factory
-  cur = conn.cursor()
-  
-  articles = cur.execute('SELECT * FROM articles').fetchall()
 
-  return jsonify(articles)
+@app.route('/all', methods=['GET'])
+def get_all():
+    conn = sqlite3.connect('articles.db')
+    conn.row_factory = dict_factory
+    cur = conn.cursor()
+
+    articles = cur.execute('SELECT * FROM articles').fetchall()
+
+    return jsonify(articles)
+
+
+@app.route('/article/<article_id>')
+def get_article(article_id):
+    conn = sqlite3.connect('articles.db')
+    conn.row_factory = dict_factory
+    cur = conn.cursor()
+
+    article = cur.execute('SELECT * FROM articles where id = ?', (article_id,)).fetchone()
+
+    return jsonify(article)
+
 
 app.run(host='0.0.0.0')
